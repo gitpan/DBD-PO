50640bb4fc273e24b37c982dbee2cd48468f61e9
Steffen Winkler <nospam{cpanMINUS94k2fp2eATsteffenMINUSwinklerDOTde}@example.com>
2009-05-29 19:27:24 +0000

    Import of STEFFENW/DBD-PO-2.07 from CPAN.
    gitpan-cpan-distribution: DBD-PO
    gitpan-cpan-version:      2.07
    gitpan-cpan-path:         STEFFENW/DBD-PO-2.07.tar.gz
    gitpan-cpan-author:       STEFFENW
    gitpan-cpan-maturity:     released

--- lib/DBD/PO.pm
+++ lib/DBD/PO.pm
@@ -3,7 +3,7 @@ package DBD::PO;
 use strict;
 use warnings;
 
-our $VERSION = '2.06';
+our $VERSION = '2.07';
 our $ATTRIBUTION = __PACKAGE__
                    . ' by Steffen Winkler <steffenw at cpan.org>';
 
@@ -34,13 +34,13 @@ __END__
 
 DBD::PO - DBI driver for PO files
 
-$Id: PO.pm 388 2009-05-21 04:36:35Z steffenw $
+$Id: PO.pm 399 2009-05-29 19:13:16Z steffenw $
 
 $HeadURL: https://dbd-po.svn.sourceforge.net/svnroot/dbd-po/trunk/DBD-PO/lib/DBD/PO.pm $
 
 =head1 VERSION
 
-2.06
+2.07
 
 =head1 SYNOPSIS
 
